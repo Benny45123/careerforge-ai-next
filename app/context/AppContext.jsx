988cfbdbdc907269bc9c2f217a011f9eb03e1dda
Benny Hinn <sunnyashish41@gmail.com>
2025-12-25 23:01:26 +0530

    cleanup: remove backend setup to maintain separate backend architecture

--- app/context/AppContext.jsx
+++ app/context/AppContext.jsx
@@ -4,8 +4,8 @@ import { createContext, useState } from "react";
 export const AppContext = createContext();
 
 export function AppProvider({ children }) {
-    const [user, setUser] = useState({name:"Hari",email:"hari@gmail.com"});
-    // const [user, setUser] = useState(null);
+    // const [user, setUser] = useState({name:"Hari",email:"hari@gmail.com"});
+    const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
     const [isOpen, setIsOpen] = useState(false);
     const [selectedDesign, setSelectedDesign] = useState(null);
